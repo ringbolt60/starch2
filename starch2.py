5471c5fec402598c6a044af94be9b692abbbbc7f
ringbolt60 <90988457+ringbolt60@users.noreply.github.com>
2023-08-30 11:50:54 +0100

    World NamedTuple implemented (much nicer!)

--- starch2.py
+++ starch2.py
@@ -10,7 +10,8 @@ import itertools
 import math
 import random
 from enum import Enum
-from typing import Tuple
+from typing import NamedTuple
+from utils import Dice
 
 import pytest
 
@@ -64,7 +65,7 @@ def get_args():
         "-s",
         "--satellite_mass",
         help="Mass of satellite in Earth masses",
-        metavar="distance au",
+        metavar="mass",
         type=float,
         default="0.0123",
     )
@@ -136,73 +137,55 @@ def main():
     """Start doing stuff here."""
 
     args = get_args()
-    name = args.name
-    world_type = args.type
-    planet_mass = args.mass
-    star_mass = args.mass_star
-    star_distance = args.distance_star
-    satellite_mass = args.satellite_mass
-    primary_distance = args.distance_primary
-    age = args.age
-    density = args.density
-    if world_type == WorldType.SATELLITE:
-        radius = calculate_radius(density, satellite_mass)
-    else:
-        radius = calculate_radius(density, planet_mass)
-    period = calc_orbital_period(
-        world_type,
-        star_mass=star_mass,
-        star_distance=star_distance,
-        planet_mass=planet_mass,
-        satellite_mass=satellite_mass,
-        primary_distance=primary_distance,
+    world = World(
+        name=args.name,
+        world_type=args.type,
+        planet_mass=args.mass,
+        star_mass=args.mass_star,
+        star_distance=args.distance_star,
+        satellite_mass=args.satellite_mass,
+        primary_distance=args.distance_primary,
+        age=args.age,
+        ecc=args.ecc,
+        density=args.density,
     )
-    ecc = args.ecc
-    rotation_period, lock = calc_rotation_period(
-        world_type,
-        star_mass=star_mass,
-        star_distance=star_distance,
-        planet_mass=planet_mass,
-        satellite_mass=satellite_mass,
-        primary_distance=primary_distance,
-        orbital_period=period,
-        radius=radius,
-        age=age,
-        ecc=ecc,
-    )
+    world = world._replace(orbital_period=calc_orbital_period(world))
+    rotation_period, lock = calc_rotation_period(world)
+    world = world._replace(rotational_period=rotation_period)
+    world = world._replace(lock=lock)
 
-    match world_type:
+    match world.world_type:
         case WorldType.LONE:
             print(
-                f"{name}\n"
-                f"{world_type.value} Age: {age:.3f} GYr\n"
-                f"Mass: {planet_mass:.3f} M♁ Density: {density:.3f} K♁ Radius: {radius:.0f} km\n"
-                f"Star Mass: {star_mass:.3f} M☉ Distance: {star_distance:.3f} AU\n"
+                f"{world.name}\n"
+                f"{world.world_type.value} Age: {world.age:.3f} GYr\n"
+                f"Mass: {world.planet_mass:.3f} M♁ Density: {world.density:.3f} K♁ Radius: {world.radius:.0f} km\n"
+                f"Star Mass: {world.star_mass:.3f} M☉ Distance: {world.star_distance:.3f} AU\n"
                 f"---\n"
-                f"Orbital Period = {period:.1f} hours\n"
-                f"Rotation Period = {rotation_period:.1f} hours ({lock})"
+                f"Orbital Period = {world.orbital_period:.1f} hours\n"
+                f"Rotation Period = {world.rotational_period:.1f} hours {world.lock.value}"
             )
         case WorldType.ORBITED:
             print(
-                f"{name}\n"
-                f"{world_type.value} Age: {age:.3f} GYr\n"
-                f"Mass: {planet_mass:.3f} M♁ Density: {density:.3f} K♁ Radius: {radius:.0f} km\n"
-                f"Star Mass: {star_mass:.3f} M☉ Distance: {star_distance:.3f} AU\n"
-                f"Satellite Mass: {satellite_mass:.3f} M♁ Distance: {primary_distance:.0f} km\n"
+                f"{world.name}\n"
+                f"{world.world_type.value} Age: {world.age:.3f} GYr\n"
+                f"Mass: {world.planet_mass:.3f} M♁ Density: {world.density:.3f} K♁ Radius: {world.radius:.0f} km\n"
+                f"Star Mass: {world.star_mass:.3f} M☉ Distance: {world.star_distance:.3f} AU\n"
+                f"Satellite Mass: {world.satellite_mass:.3f} M♁ Distance: {world.primary_distance:.0f} km\n"
                 f"---\n"
-                f"Orbital Period = {period:.1f} hours\n"
-                f"Rotation Period = {rotation_period:.1f} hours ({lock})"
+                f"Orbital Period = {world.orbital_period:.1f} hours\n"
+                f"Rotation Period = {world.rotational_period:.1f} hours {world.lock.value}"
             )
         case WorldType.SATELLITE:
             print(
-                f"{name}\n"
-                f"{world_type.value} Age: {age:.3f} GYr\n"
-                f"Mass: {satellite_mass:.3f} M♁ Density: {density:.3f} K♁ Radius: {radius:.0f} km\n"
-                f"Star Mass: {star_mass:.3f} M☉ Distance: {star_distance:.3f} AU\n"
-                f"Primary Mass: {planet_mass:.3f} M♁ Distance: {primary_distance:.0f} km\n"
+                f"{world.name}\n"
+                f"{world.world_type.value} Age: {world.age:.3f} GYr\n"
+                f"Mass: {world.satellite_mass:.3f} M♁ Density: {world.density:.3f} K♁ Radius: {world.radius:.0f} km\n"
+                f"Star Mass: {world.star_mass:.3f} M☉ Distance: {world.star_distance:.3f} AU\n"
+                f"Primary Mass: {world.planet_mass:.3f} M♁ Distance: {world.primary_distance:.0f} km\n"
                 f"---\n"
-                f"Orbital Period = {period:.1f} hours\n"
-                f"Rotation Period = {rotation_period:.1f} hours ({lock})"
+                f"Orbital Period = {world.orbital_period:.1f} hours\n"
+                f"Rotation Period = {world.rotational_period:.1f} hours {world.lock.value}"
             )
 
 
@@ -213,172 +196,166 @@ class WorldType(Enum):
     SATELLITE = "Satellite"
 
 
+class Resonance(Enum):
+    NONE = ""
+    LOCK_TO_SATELLITE = "1:1 tidal lock with satellite"
+    LOCK_TO_PRIMARY = "1:1 tidal lock with planet"
+    LOCK_TO_STAR = "1:1 tidal lock with star"
+    RESONANCE_3_2 = "3:2 resonance with star"
+    RESONANCE_2_1 = "2:1 resonance with star"
+    RESONANCE_5_2 = "5:2 resonance with star"
+    RESONANCE_3_1 = "3:1 resonance with star"
+
+
 # --------------------------------------------------
-def calc_orbital_period(
-    world_type: WorldType,
-    planet_mass: float = 1.0,
-    star_mass: float = 1.0,
-    star_distance: float = 1.0,
-    satellite_mass: float = 0.0123,
-    primary_distance: float = 384400.0,
-) -> float:
+class World(NamedTuple):
+    name: str = "DEFAULT"
+    world_type: WorldType = WorldType.LONE
+    planet_mass: float = 1.0
+    star_mass: float = 1.0
+    star_distance: float = 1.0
+    satellite_mass: float = 0.0123
+    primary_distance: float = 384400
+    age: float = 4.568
+    ecc: float = 0.07
+    orbital_period: float = 7617.0
+    density: float = 1.0
+    rotational_period: float = 24.0
+    lock: Resonance = Resonance.NONE
+
+    @property
+    def radius(self):
+        mass = (
+            self.satellite_mass
+            if self.world_type is WorldType.SATELLITE
+            else self.planet_mass
+        )
+        return int(round(6738 * math.pow(mass / self.density, 1.0 / 3.0), 0))
+
+    @property
+    def t(self) -> float:
+        t = 0
+        if self.world_type is WorldType.LONE:
+            t = (
+                9.6e-14
+                * self.age
+                * math.pow(self.star_mass, 2)
+                * math.pow(self.radius, 3)
+                / self.planet_mass
+                / math.pow(self.star_distance, 6)
+            )
+        elif self.world_type is WorldType.ORBITED:
+            t = (
+                1e25
+                * self.age
+                * math.pow(self.satellite_mass, 2)
+                * math.pow(self.radius, 3)
+                / self.planet_mass
+                / math.pow(self.primary_distance, 6)
+            )
+        return t
+
+
+# --------------------------------------------------
+def calc_orbital_period(w: "World") -> float:
     """
     Returns orbital period around primary in hours.
 
     Implements Step 18 pp 92,93. Constants tweaked to make earth and Luna exact.
     """
-    if world_type is WorldType.SATELLITE:
+    if w.world_type is WorldType.SATELLITE:
         return 2.768e-6 * math.sqrt(
-            math.pow(primary_distance, 3) / (satellite_mass + planet_mass)
+            math.pow(w.primary_distance, 3) / (w.satellite_mass + w.planet_mass)
         )
     else:
-        return 8766.0 * math.sqrt(math.pow(star_distance, 3) / star_mass)
+        return 8766.0 * math.sqrt(math.pow(w.star_distance, 3) / w.star_mass)
 
 
 # --------------------------------------------------
 def test_orbital_period():
     """Checks period calculated correctly"""
-    period_lone = calc_orbital_period(
+
+    lone_input = World(
         world_type=WorldType.LONE, planet_mass=0.93, star_mass=0.94, star_distance=0.892
     )
+    period_lone = calc_orbital_period(lone_input)
     assert period_lone == pytest.approx(7617.0, abs=1e-1)
 
-    period_satellite = calc_orbital_period(
+    satellite_input = World(
         world_type=WorldType.SATELLITE,
         satellite_mass=0.023,
-        primary_distance=175845,
         planet_mass=0.876,
+        primary_distance=175845,
     )
+    period_satellite = calc_orbital_period(satellite_input)
     assert period_satellite == pytest.approx(215.3, abs=1e-1)
 
 
 # --------------------------------------------------
-class Dice:
-    """Generator producing a stream of six sided dice rolls."""
-
-    def __init__(self, seed: int | None = None, mocks=None):
-        self.seed: int | None = seed
-        if mocks:
-            self.mocks = itertools.cycle(mocks)
-        else:
-            self.mocks = None  # type: ignore
-        self.generator = random.Random(self.seed)
-
-    def next(self):
-        if self.mocks:
-            return self.mocks.__next__()
-        else:
-            return self.generator.randint(1, 6)
-
-
-# --------------------------------------------------
-def test_dice():
-    d6 = Dice()
-    total = 0
-    for _ in range(1000):
-        roll = d6.next()
-        assert 1 <= roll <= 6
-        total += roll
-    assert 1000 <= total <= 6000
-    assert total / 1000 == pytest.approx(3.5, abs=0.5)
-
-    d6_seeded = Dice(seed=1)
-    expected = [2, 5, 1, 3, 1, 4, 4, 4, 6, 4]
-    actual = []
-    for _ in range(10):
-        actual.append(d6_seeded.next())
-    assert actual == expected
-
-    d6_mock = Dice(mocks=[1, 2, 3])
-    expected = [1, 2, 3, 1, 2, 3, 1]
-    actual = []
-    for _ in range(7):
-        actual.append(d6_mock.next())
-    assert actual == expected
-
-
-# --------------------------------------------------
-def calc_rotation_period(
-    world_type: WorldType,
-    planet_mass: float = 1.0,
-    star_mass: float = 1.0,
-    star_distance: float = 1.0,
-    satellite_mass: float = 0.0123,
-    primary_distance: float = 384400.0,
-    orbital_period: float = 7617.0,
-    age: float = 4.568,
-    radius: float = 6378.1,
-    ecc: float = 0.0,
-    rand: Dice = Dice(),
-) -> (float, str):
+def calc_rotation_period(w: World, rand: Dice = Dice()) -> (float, Resonance):
     """
     Returns sidereal rotation period of world.
 
     Implements Step 19 pp 93-95. Constants tweaked to make earth and Luna exact.
     """
     roll = sum([rand.next() for _ in range(3)])
-    if world_type is WorldType.SATELLITE:
-        return orbital_period, "1:1 tidal lock with primary"
+    if w.world_type is WorldType.SATELLITE:
+        return w.orbital_period, Resonance.LOCK_TO_PRIMARY
 
-    if world_type is WorldType.LONE:
-        t = (
-            9.6e-14
-            * age
-            * math.pow(star_mass, 2)
-            * math.pow(radius, 3)
-            / planet_mass
-            / math.pow(star_distance, 6)
-        )
-    elif world_type is WorldType.ORBITED:
-        t = (
-            1e25
-            * age
-            * math.pow(satellite_mass, 2)
-            * math.pow(radius, 3)
-            / planet_mass
-            / math.pow(primary_distance, 6)
-        )
-    t_adjusted = int(round(t * 12, 0) + roll)
-    if t >= 2 or t_adjusted >= 24:
-        if world_type is WorldType.LONE:
-            period = orbital_period
-            lock, period = adjust_for_eccentricity(ecc, period)
+    t_adjusted = int(round(w.t * 12, 0) + roll)
+    if w.t >= 2 or t_adjusted >= 24:
+        if w.world_type is WorldType.LONE:
+            period = w.orbital_period
+            lock, period = adjust_for_eccentricity(w.ecc, period)
             return period, lock
         else:
             return (
                 2.768e-6
                 * math.sqrt(
-                    math.pow(primary_distance, 3) / (satellite_mass + planet_mass)
+                    math.pow(w.primary_distance, 3) / (w.satellite_mass + w.planet_mass)
                 ),
-                "1:1 tidal lock with satellite",
+                Resonance.LOCK_TO_SATELLITE,
             )
     else:
         p = look_up(planet_rotation_rate, t_adjusted)
         lower, upper = p
         period = random.uniform(lower, upper)
-        lock = ""
-        if period >= orbital_period:
-            period = orbital_period
-            lock, period = adjust_for_eccentricity(ecc, period)
+        lock = Resonance.NONE
+        if period >= w.orbital_period:
+            period = w.orbital_period
+            lock, period = adjust_for_eccentricity(w.ecc, period)
         return period, lock
 
 
 # --------------------------------------------------
 def test_rotation_period():
     """Checks rotation period calculated correctly"""
-    period_lone = calc_rotation_period(
+    lone_world_1 = World(
         world_type=WorldType.LONE,
         planet_mass=0.93,
         star_mass=0.94,
         star_distance=0.892,
         age=3.225,
-        radius=6584.0,
         ecc=0.07,
-        rand=Dice(mocks=[3, 6, 1]),
     )
-    assert 24 <= period_lone[0] <= 40
+    period, lock = calc_rotation_period(lone_world_1, rand=Dice(mocks=[3, 6, 1]))
+    assert 24 <= period <= 40
+    assert lock == Resonance.NONE
 
-    period_orbited = calc_rotation_period(
+    lone_world_2 = World(
+        world_type=WorldType.LONE,
+        planet_mass=0.93,
+        star_mass=1.256,
+        star_distance=0.892,
+        age=9.225,
+        ecc=0.37,
+        orbital_period=6589.5,
+    )
+    period, lock = calc_rotation_period(lone_world_2, rand=Dice(mocks=[3, 6, 5]))
+    assert period == pytest.approx(2635.8, abs=1e-1)
+    assert lock == Resonance.RESONANCE_5_2
+
+    orbited_world = World(
         world_type=WorldType.ORBITED,
         planet_mass=0.93,
         star_mass=0.14,
@@ -386,82 +363,68 @@ def test_rotation_period():
         primary_distance=125687,
         satellite_mass=0.025,
         age=1.225,
-        radius=6584.0,
         ecc=0.07,
-        rand=Dice(mocks=[1, 2, 1]),
     )
-    period, lock = period_orbited
+    period, lock = calc_rotation_period(orbited_world, rand=Dice(mocks=[1, 2, 1]))
     assert period == pytest.approx(126.2, abs=1e-1)
-    assert lock == "1:1 tidal lock with satellite"
+    assert lock == Resonance.LOCK_TO_SATELLITE
 
-    results = calc_rotation_period(
+    satellite_world = World(
         world_type=WorldType.SATELLITE,
         satellite_mass=0.023,
         primary_distance=175845,
         planet_mass=0.876,
         orbital_period=215.3,
     )
+    results = calc_rotation_period(satellite_world)
     period, lock = results
     assert period == pytest.approx(215.3, abs=1e-1)
-    assert lock == "1:1 tidal lock with primary"
-
-
-# --------------------------------------------------
-def calculate_radius(density: float = 1.0, mass: float = 1) -> int:
-    """Calculates world radius from mass and density"""
-    return int(round(6738 * math.pow(mass / density, 1.0 / 3.0), 0))
-
-
-# --------------------------------------------------
-def test_calculate_radius():
-    """Checks world radius calculated correctly from mass and density"""
-    radius = calculate_radius(mass=0.931, density=1.122)
-    assert radius == 6332
+    assert lock == Resonance.LOCK_TO_PRIMARY
 
 
 # --------------------------------------------------
 def adjust_for_eccentricity(ecc=0.0, period=1.0):
     multiplier = 1
-    text = ""
+    lock = ""
     if ecc <= 0.12:
-        text = "1:1 tidal lock with star"
+        lock = Resonance.LOCK_TO_STAR
     elif 0.12 < ecc < 0.25:
         multiplier = 2.0 / 3.0
-        text = "3:2 spin resonance with star"
+        lock = Resonance.RESONANCE_3_2
     elif 0.25 <= ecc < 0.35:
         multiplier = 0.5
-        text = "2:1 spin resonance with star"
+        lock = Resonance.RESONANCE_2_1
     elif 0.35 <= ecc < 0.45:
         multiplier = 0.4
-        text = "5:2 spin resonance with star"
+        lock = Resonance.RESONANCE_5_2
     else:
-        text = "3:1 spin resonance with star"
+        lock = Resonance.RESONANCE_3_1
         multiplier = 1.0 / 3.0
-    return text, period * multiplier
+    return lock, period * multiplier
 
 
 # --------------------------------------------------
 @pytest.mark.parametrize(
-    "e, p, expected_period, expected_text",
+    "e, p, expected_period, expected_lock",
     [
-        (0.01, 256.0, 256.0, "1:1 tidal lock with star"),
-        (0.08, 478.0, 478.0, "1:1 tidal lock with star"),
-        (0.18, 330.0, 220.0, "3:2 spin resonance with star"),
-        (0.25, 550.0, 275.0, "2:1 spin resonance with star"),
-        (0.29, 700.0, 350.0, "2:1 spin resonance with star"),
-        (0.35, 500.0, 200.0, "5:2 spin resonance with star"),
-        (0.4, 1000.0, 400.0, "5:2 spin resonance with star"),
-        (0.45, 300.0, 100.0, "3:1 spin resonance with star"),
-        (0.6, 600.0, 200.0, "3:1 spin resonance with star"),
+        (0.01, 256.0, 256.0, Resonance.LOCK_TO_STAR),
+        (0.08, 478.0, 478.0, Resonance.LOCK_TO_STAR),
+        (0.18, 330.0, 220.0, Resonance.RESONANCE_3_2),
+        (0.25, 550.0, 275.0, Resonance.RESONANCE_2_1),
+        (0.29, 700.0, 350.0, Resonance.RESONANCE_2_1),
+        (0.35, 500.0, 200.0, Resonance.RESONANCE_5_2),
+        (0.4, 1000.0, 400.0, Resonance.RESONANCE_5_2),
+        (0.45, 300.0, 100.0, Resonance.RESONANCE_3_1),
+        (0.6, 600.0, 200.0, Resonance.RESONANCE_3_1),
     ],
 )
-def test_calculate_resonance(e, p, expected_period, expected_text):
+def test_calculate_resonance(e, p, expected_period, expected_lock):
     """Checks resonant orbital periods adjusted for eccentricity"""
 
     result = adjust_for_eccentricity(ecc=e, period=p)
-    text, period = result
+    lock, period = result
     assert period == expected_period
-    assert text == expected_text
+    assert lock == expected_lock
 
 
 # --------------------------------------------------
